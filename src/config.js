fedd8a53e06540d95e72ecb132f14888df2196cc
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-02-23 19:57:58 +0530

    vercel url added

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'https://assignment-pro.onrender.com/';
+const API_BASE_URL = 'https://assignment-pro-ebon.vercel.app/';
+// const API_BASE_URL = 'https://assignment-pro.onrender.com/';
 // const API_BASE_URL = 'http://localhost:5000/';
 
 export default API_BASE_URL;
