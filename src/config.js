69244601e54ad053c493e9a004bc3036447fd26a
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-02-23 20:02:46 +0530

    url fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'https://assignment-pro-ebon.vercel.app/';
-// const API_BASE_URL = 'https://assignment-pro.onrender.com/';
+const API_BASE_URL = 'https://assignment-pro.onrender.com/';
 // const API_BASE_URL = 'http://localhost:5000/';
 
 export default API_BASE_URL;
